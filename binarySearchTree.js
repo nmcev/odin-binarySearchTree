d7d08fed6e537839761aab3018b2d88c4ab6033a
muha <1student.muhammed1@gmail.com>
2023-11-25 13:53:38 +0300

    feat: buildTree func and prettyPrint func added

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,30 @@ class Tree {
     constructor(array) {
         this.root = this.buildTree(array, 0, array.length - 1)
     }
+
+    buildTree(array, start, end) {
+
+        if (start > end) return null;
+
+        let mid = Math.floor((start + end) / 2);
+        let root = new Node(array[mid]);
+
+        root.left = this.buildTree(array, start, mid - 1);
+        root.right = this.buildTree(array, mid + 1, end);
+        
+        return root;
+    }
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
